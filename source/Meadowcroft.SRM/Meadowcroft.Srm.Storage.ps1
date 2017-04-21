f7ee8918ddfb8cb4a0a691577e07078ee7203660
Ben Meadowcroft <ben@benmeadowcroft.com>
2017-04-20 20:59:03 -0700

    Added discover devices function

---
+++ source/Meadowcroft.SRM/Meadowcroft.Srm.Storage.ps1
@@ -0,0 +1,14 @@
+﻿
+<#
+.SYNOPSIS
+Trigger Discover Devices for Site Recovery Manager
+
+.PARAMETER ProtectionGroup
+Return discover devices task
+#>
+Function Start-DiscoverDevices {
+    $api = Get-ServerApiEndpoint
+    [VMware.VimAutomation.Srm.Views.DiscoverDevicesTask] $task = $api.Storage.DiscoverDevices()
+
+    return $task
+}
